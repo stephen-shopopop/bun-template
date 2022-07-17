70c1095b639a763a2bb40df38bd5addb5537ac1f
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2022-07-17 19:25:30 +0200

    feat(bundle): ADD typescript, standard for lint & typecheck tools

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'bun:test'
+
+describe('Example', () => {
+  it('should work', () => {
+    expect(1).toBe(1)
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 Bun.serve({
-  fetch(req: Request) {
-    return new Response(`Echo: ${req.url}`);
+  fetch (req: Request) {
+    return new Response(`Echo: ${req.url}`)
   },
 
   // baseURI: "http://localhost:3000",
@@ -11,15 +11,15 @@ Bun.serve({
   // },
 
   // this boolean enables bun's default error handler
-  development: process.env.NODE_ENV !== "production",
+  development: process.env.NODE_ENV !== 'production',
   // note: this isn't node, but for compatibility bun supports process.env + more stuff in process
 
   // SSL is enabled if these two are set
   // certFile: './cert.pem',
   // keyFile: './key.pem',
 
-  port: 3000, // number or string
-});
+  port: 3000 // number or string
+})
 // Start a fast HTTP server from the main file's export
 // export default {
 //   fetch(req) {
